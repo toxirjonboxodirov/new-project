63667d1c52e61e5bf020ea58a97375cf41c2d7a5
unknown <you@example.com>
2023-01-26 21:21:44 +0300

    project work

---
+++ 3-lesson JS/main.js
@@ -0,0 +1,34 @@
+/*var num1 = prompt('enter the value')
+var num2 = prompt('displays its degree')
+
+var c = Math.pow(num1,num2)
+
+console.log(c)*/
+//var isOnline = false
+
+//after 'if' we use boolen values when 'true' is used it displays true's value if false its opposite of true
+/*if(true){
+console.log('isOnline')
+}else{
+    console.log('last seen lately')
+}*/
+var sum1 = prompt('Name');
+var sum2 = prompt('Password');
+
+var sum3 = prompt('Taxir');
+var sum4 = prompt(2209);
+
+if(sum1=sum3){
+    confirm('this name is true');
+}else{
+    confirm('this name is not defined');
+}
+if(sum2=sum4){
+    confirm('this password is true');
+}else{
+    confirm('this password is not defined');
+}
+
+
+
+
---
+++ 4-lesson 'if-else'/main.js
@@ -0,0 +1,47 @@
+/*var mylogin = 'Simon'
+var worker = 'Tim'
+var login = prompt('Yourlogin')
+
+if(mylogin===login){
+    console.log('success')
+}else if(login==worker){
+    console.log('good but a bit incorrect')
+}else{
+    console.log('incorrect')
+}*/
+/*var son = prompt('son')*1
+switch (son) {
+    case 4: console.log('good')
+        break;
+    case 3: console.log('better')
+        break;
+    case 2: console.log('best')
+        break;
+    case 1: console.log('solid')
+        break;
+    default: console.error('inccorect')
+        break;
+}*/
+/*a===1 ? console.log(totallytrue):
+a===2 ? console.log(maybetrue):
+console.log('untrue')*/
+
+
+/*var a = prompt('enter a value')
+if(9<=a){
+    console.log('Larger')
+}else{
+    console.log('smaller')
+}*/
+
+
+var b = prompt('enter a value')
+switch (21<=b) {
+    case 21:
+        confirm('larger')
+        break;
+
+    default:
+        confirm('smaller')
+        break;
+}
